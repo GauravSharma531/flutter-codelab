2b1606a7b199fe30a246d3b2ce3e0d6c66068161
gaurav <gauravsharma531@gmail.com>
2018-05-26 01:12:40 +0530

    Demo - Rectangle + Center aligned text
    *) Created a material app
    *) Created a toolbar
    *) Created a rectangle which contains text in center.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(HelloRectangle());
+void main() => runApp(MaterialApp(
+      title: "Hello Rectangle",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Rectangle"),
+        ),
+        body: HelloRectangle(),
+      ),
+    ));
 
-class HelloRectangle extends StatelessWidget{
-
+class HelloRectangle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-      child: Container(width: 400.0,height: 400.0,
-          color: Colors.deepOrange,),
+      child: Container(
+        width: 400.0,
+        height: 400.0,
+        color: Colors.deepOrange,
+        child: Center(
+            child: Text(
+          'Hello Bharath, How are you ?',
+          style: TextStyle(
+            color: Colors.blueAccent,
+            fontSize: 40.0,
+
+          ),
+          textAlign: TextAlign.center,
+
+        )),
+      ),
     );
   }
-
 }
-
-
